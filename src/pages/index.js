5417d2f6fcb612777b390bf869ad7069f9b0b1d1
vamshidhar <vamshidhar490@outlook.com>
2018-08-09 10:56:41 +0530

    Add new skill angular

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,29 @@
-import React from "react";
-import Link from "gatsby-link";
-import Typist from "react-typist";
-import { withPrefix } from "gatsby-link";
-import Badges from "../components/Badges";
+import React from 'react';
+import Link from 'gatsby-link';
+import Typist from 'react-typist';
+import { withPrefix } from 'gatsby-link';
+import Badges from '../components/Badges';
 
-import SimpleIcon from "../components/SimpleIcon";
+import SimpleIcon from '../components/SimpleIcon';
 
-import "react-typist/dist/Typist.css";
+import 'react-typist/dist/Typist.css';
 
 class IndexPage extends React.Component {
   constructor() {
     super();
   }
   trackCta() {
-    if (typeof window !== "undefined" && window.ga) {
-      window.ga("send", "event", "Contact", "Click CTA to go to contact page");
+    if (typeof window !== 'undefined' && window.ga) {
+      window.ga('send', 'event', 'Contact', 'Click CTA to go to contact page');
     }
   }
   componentDidMount() {
     if (typeof window !== `undefined`) {
-      if (window.location.pathname === "/") {
-        if (navigator.language === "pt-BR" || navigator.language === "pt") {
-          window.___history.replace("/pt/");
+      if (window.location.pathname === '/') {
+        if (navigator.language === 'pt-BR' || navigator.language === 'pt') {
+          window.___history.replace('/pt/');
         } else {
-          window.___history.replace("/en/");
+          window.___history.replace('/en/');
         }
       }
     }
@@ -67,58 +67,64 @@ class IndexPage extends React.Component {
             <Badges
               skills={[
                 {
-                  icon: "react",
-                  name: "React",
-                  url: "https://reactjs.org/",
-                  color: "#61DAFB"
+                  icon: 'react',
+                  name: 'React',
+                  url: 'https://reactjs.org/',
+                  color: '#61DAFB'
                 },
                 {
-                  icon: "redux",
-                  name: "Redux",
-                  url: "https://redux.js.org/",
-                  color: "#61DAFB"
+                  icon: 'angular',
+                  name: 'Angular',
+                  url: 'https://angular.io/',
+                  color: '#61DAFB'
                 },
                 {
-                  icon: "nodejs",
-                  name: "Node.js",
-                  url: "https://nodejs.org/",
-                  color: "#61DAFB"
+                  icon: 'redux',
+                  name: 'Redux',
+                  url: 'https://redux.js.org/',
+                  color: '#61DAFB'
                 },
                 {
-                  icon: "graphql",
-                  name: "GraphQL",
-                  url: "https://graphql.org/",
-                  color: "#61DAFB"
+                  icon: 'node-dot-js',
+                  name: 'Node.js',
+                  url: 'https://nodejs.org/',
+                  color: '#61DAFB'
                 },
                 {
-                  icon: "mongodb",
-                  name: "mongoDB",
-                  url: "https://mongodb.com/",
-                  color: "#61DAFB"
+                  icon: 'graphql',
+                  name: 'GraphQL',
+                  url: 'https://graphql.org/',
+                  color: '#61DAFB'
                 },
                 {
-                  icon: "mysql",
-                  name: "MySQL",
-                  url: "https://mysql.com/",
-                  color: "#61DAFB"
+                  icon: 'mongodb',
+                  name: 'mongoDB',
+                  url: 'https://mongodb.com/',
+                  color: '#61DAFB'
                 },
                 {
-                  icon: "sass",
-                  name: "Sass",
-                  url: "https://sass-lang.com/",
-                  color: "#61DAFB"
+                  icon: 'mysql',
+                  name: 'MySQL',
+                  url: 'https://mysql.com/',
+                  color: '#61DAFB'
                 },
                 {
-                  icon: "gatsby",
-                  name: "Gatsby",
-                  url: "https://gatsbyjs.org/",
-                  color: "#61DAFB"
+                  icon: 'sass',
+                  name: 'Sass',
+                  url: 'https://sass-lang.com/',
+                  color: '#61DAFB'
                 },
                 {
-                  icon: "git",
-                  name: "Git",
-                  url: "https://git-scm.com/",
-                  color: "#61DAFB"
+                  icon: 'gatsby',
+                  name: 'Gatsby',
+                  url: 'https://gatsbyjs.org/',
+                  color: '#61DAFB'
+                },
+                {
+                  icon: 'git',
+                  name: 'Git',
+                  url: 'https://git-scm.com/',
+                  color: '#61DAFB'
                 }
               ]}
             />
